7a595bf0757a434f35bc0b5e64c98fd378d68d23
condesahreyes <hreyes.condesa@gmail.com>
2024-11-29 17:07:22 -0300

    First commit

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,34 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>PATO MARKETING</h1>
+                <p>𝐈𝐝𝐞𝐧𝐭𝐢𝐝𝐚𝐝 𝐠𝐫𝐚́𝐟𝐢𝐜𝐚:
+Define el aspecto visual de la marca, “todo lo que se ve”. La empresa tiene una identidad gráfica propia cuando identificamos fácilmente su presencia y la relacionamos con un logotipo / color / tipografía.
+
+Nos lleva a trabajar con el 𝐁𝐫𝐚𝐧𝐝𝐢𝐧𝐠:
+Fortalece la marca ya existente, sumando a la identidad gráfica sentimientos y experiencias que el público genera con la empresa.
+
+
+𝐈𝐝𝐞𝐧𝐭𝐢𝐝𝐚𝐝 𝐠𝐫𝐚́𝐟𝐢𝐜𝐚:
+Precio único: $4500
+
+Incluye: 
+-̞ Logo.
+-̞ Tipografía.
+-̞ Palea de colores.
+-̞ Piezas para avatar y perfil de Instagram
+-̞ Estilo de ilustración.
+-̞ Íconos.
+
+
+𝐂𝐨𝐧𝐭𝐞𝐧𝐢𝐝𝐨 𝐩𝐚𝐫𝐚 𝐫𝐞𝐝𝐞𝐬:
+𝘗𝘢𝘤𝘬 𝘮𝘦𝘯𝘴𝘶𝘢𝘭 𝘥𝘦 𝘥𝘪𝘴𝘦ñ𝘰 𝘥𝘦 𝘱𝘪𝘦𝘻𝘢𝘴 𝘷𝘪𝘴𝘶𝘢𝘭𝘦𝘴
+Precio mensual: US$160.
+
+Incluye:
+-̞ 8 a 10 postes.
+-̞ 12 a 16 estores.
+-̞ Redacción para posts..</p>
             </div>
         </div>
         <nav>
